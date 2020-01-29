c5a297745fe4ad4cbf8f1cc4fa7098b9fb74a9b3
Raphael Fleury <raphael.fleury@b2wdigital.com>
2020-01-29 17:06:15 -0300

    Criando classe do Async Task

--- Top10DownloadApp/app/src/main/java/com/example/top10downloadapp/MainActivity.kt
+++ Top10DownloadApp/app/src/main/java/com/example/top10downloadapp/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.top10downloadapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+        val downloadData = DownloadData()
+        downloadData.execute("URL goes here")
+        Log.d(TAG,"onCreate: Done")
+    }
+
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadDAta"
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute: paramater is $result")
+            }
+
+            override fun doInBackground(vararg params: String?): String {
+                Log.d(TAG, "doInBackgorun: starts with ${params[0]}")
+                return "doInBackgorund completed"
+            }
+
+        }
     }
 }
