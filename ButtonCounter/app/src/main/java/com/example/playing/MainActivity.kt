2d8052fd62e957516fc99d915a4a98ef2f7b00d9
Raphael Fleury <raphael.fleury@b2wdigital.com>
2020-01-29 12:28:16 -0300

    Salvando o estado dos textos

--- ButtonCounter/app/src/main/java/com/example/playing/MainActivity.kt
+++ ButtonCounter/app/src/main/java/com/example/playing/MainActivity.kt
@@ -3,32 +3,46 @@ package com.example.playing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENTS = "TextContent"
 
 class MainActivity : AppCompatActivity() {
 
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: Called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById(R.id.editText)
-        button = findViewById(R.id.button)
+        val userInput: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
         textView?.text = ""
         textView?.movementMethod = ScrollingMovementMethod()
 
-        button?.setOnClickListener {
-            textView?.append(userInput?.text)
-            textView?.append("\n")
+        button.setOnClickListener {
+            Log.d(TAG, "onCLick: Called")
+            textView?.text = userInput.text
+            userInput.text.clear()
         }
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView?.text = savedInstanceState.getString(TEXT_CONTENTS, "")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(TEXT_CONTENTS, textView?.text.toString())
     }
 }
